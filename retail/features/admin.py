71d0e4326c860b5cb3dca4c63a906087cfe888a4
lucaslinhares <lucaslinhares027@gmail.com>
2024-09-11 17:54:58 -0300

    Add validations related to definition

--- retail/features/admin.py
+++ retail/features/admin.py
@@ -20,6 +20,35 @@ class FeatureVersionInlineForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    def clean_definition(self):
+        definition = self.cleaned_data.get("definition")
+
+        flows = definition.get("flows")
+        if not flows:
+            raise forms.ValidationError(
+                "O atributo 'flows' é obrigatório e não foi encontrado no JSON."
+            )
+
+        flows_name = []
+
+        for flow in flows:
+            if flow.get("name") in flows_name:
+                raise forms.ValidationError(
+                    "Não é possivel ter mais de um fluxo com o mesmo nome"
+                )
+            flows_name.append(flow.get("name"))
+
+            nodes = flow.get("nodes", [])
+            for node in nodes:
+                actions = node.get("actions", [])
+                for action in actions:
+                    if action.get("type") == "open_ticket" and action.get("assignee"):
+                        raise forms.ValidationError(
+                            "O campo 'assignee' não pode ser preenchido em actions do tipo 'open_ticket'."
+                        )
+
+        return definition
+
     def save(self, commit: bool) -> FeatureVersion:
         feature_version: FeatureVersion = super().save(commit)
         feature = feature_version.feature
@@ -33,10 +62,10 @@ class FeatureVersionInlineForm(forms.ModelForm):
                     self.instance.definition["flows"].append(flow)
 
                 for campaign in function_version.definition["campaigns"]:
-                    self.instance.defintion["campaigns"].append(campaign)
+                    self.instance.definition["campaigns"].append(campaign)
 
                 for trigger in function_version.definition["triggers"]:
-                    self.instance.defintion["triggers"].append(trigger)
+                    self.instance.definition["triggers"].append(trigger)
 
                 for field in function_version.definition["fields"]:
                     self.instance.definition["fields"].append(field)
