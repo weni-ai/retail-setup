b58525cca306abc3017564a7aa1e249d66d2961b
lucaslinhares <lucaslinhares027@gmail.com>
2024-09-20 14:59:01 -0300

    Endpoint to get features

---
+++ retail/api/features/views.py
@@ -0,0 +1,32 @@
+from rest_framework import views, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+
+from retail.api.features.serializers import FeaturesSerializer
+from retail.features.models import Feature, IntegratedFeature
+
+
+class FeaturesView(views.APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, project_uuid):
+        try:
+
+            category = request.query_params.get("category", None)
+            integrated_features = IntegratedFeature.objects.filter(
+                project=project_uuid
+            ).values_list("feature__uuid", flat=True)
+
+            features = Feature.objects.exclude(uuid__in=integrated_features)
+
+            if category:
+                features = features.filter(category=category)
+
+            serializer = FeaturesSerializer(features, many=True)
+
+            return Response({"results": serializer.data}, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
--- retail/api/routers.py
+++ retail/api/routers.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from rest_framework import routers
+
+from retail.api.features.views import FeaturesView
 
 from .integrated_feature.views import IntegratedFeatureView
 
@@ -8,6 +9,7 @@ urlpatterns = [
     path(
         "feature/<uuid:feature_uuid>/integrate/",
         IntegratedFeatureView.as_view(),
-        name="integrated_feature"
-    )
+        name="integrated_feature",
+    ),
+    path("feature/<uuid:project_uuid>/", FeaturesView.as_view(), name="features"),
 ]
