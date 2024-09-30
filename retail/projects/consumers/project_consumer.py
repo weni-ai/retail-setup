b19e2f0771bdfbefabb2aa39109caec25eba1e69
elitonzky <eliton-jorge@hotmail.com>
2024-09-30 10:56:37 -0300

    Remove unnecessary try-exception

--- retail/projects/consumers/project_consumer.py
+++ retail/projects/consumers/project_consumer.py
@@ -10,21 +10,13 @@ from weni.eda.django.consumers import EDAConsumer
 class ProjectConsumer(EDAConsumer):  # pragma: no cover
     def consume(self, message: amqp.Message):
         print(f"[ProjectConsumer] - Consuming a message. Body: {message.body}")
+        body = JSONParser.parse(message.body)
 
-        try:
-            body = JSONParser.parse(message.body)
-
-            project_dto = ProjectCreationDTO(
-                uuid=body.get("uuid"),
-                name=body.get("name"),
-                organization_uuid=body.get("organization_uuid"),
-                authorizations=body.get("authorizations", []),
-            )
-
-            ProjectCreationUseCase.create_project(project_dto)
-
-            message.channel.basic_ack(message.delivery_tag)
-
-        except Exception as exception:
-            message.channel.basic_reject(message.delivery_tag, requeue=False)
-            print(f"[ProjectConsumer] - Message rejected by: {exception}")
+        project_dto = ProjectCreationDTO(
+            uuid=body.get("uuid"),
+            name=body.get("name"),
+            organization_uuid=body.get("organization_uuid"),
+            authorizations=body.get("authorizations", []),
+        )
+        ProjectCreationUseCase.create_project(project_dto)
+        self.ack()
