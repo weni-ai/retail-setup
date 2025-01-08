626dff37edc10b739b119d885f11b94a8b236b57
elitonzky <eliton-jorge@hotmail.com>
2025-01-08 14:57:54 -0300

    feat: add service to create template message on integrations engine

--- retail/interfaces/clients/integrations/interface.py
+++ retail/interfaces/clients/integrations/interface.py
@@ -5,3 +5,15 @@ class IntegrationsClientInterface(ABC):
     @abstractmethod
     def get_vtex_integration_detail(self, project_uuid: str):
         pass
+
+    @abstractmethod
+    def create_template_message(
+        self, app_uuid: str, project_uuid: str, name: str, category: str
+    ) -> str:
+        pass
+
+    @abstractmethod
+    def create_template_translation(
+        self, app_uuid: str, project_uuid: str, template_uuid: str, payload: dict
+    ) -> dict:
+        pass
