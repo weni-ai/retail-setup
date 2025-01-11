6a2e354785b89704082effcb1b0afab62fb57597
elitonzky <eliton-jorge@hotmail.com>
2025-01-10 21:46:08 -0300

    fix: add project models file

--- retail/projects/models.py
+++ retail/projects/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
     name = models.CharField(max_length=256)
     uuid = models.UUIDField()
     organization_uuid = models.UUIDField(null=True)
-    vtex_account = models.CharField(max_length=100, null=True)
+    vtex_account = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self) -> str:
         return self.name
