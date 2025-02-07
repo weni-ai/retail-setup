1f6b67a4754d2e7b417cdc87e8f1b5d0da849909
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-02-07 14:35:16 -0300

    feat: add order status fields validations

--- retail/webhooks/vtex/usecases/order_status.py
+++ retail/webhooks/vtex/usecases/order_status.py
@@ -1,3 +1,6 @@
+from retail.features.models import IntegratedFeature
+
+
 class OrderStatusUseCase:
     """
     Use case for handling order status updates.
@@ -9,3 +12,23 @@ class OrderStatusUseCase:
         Get the domain for a given account.
         """
         return f"https://{account}.vtexcommercestable.com.br"
+
+    @classmethod
+    def get_template_by_order_status(
+        cls, integrated_feature: IntegratedFeature, order_status: str
+    ):
+        """
+        Get the template for a given order status.
+        """
+        order_status_templates = integrated_feature.config.get(
+            "order_status_templates", {}
+        )
+
+        return order_status_templates.get(order_status, "")
+
+    @classmethod
+    def process_notification(self, domain: str, order_id: str, template: str):
+        """
+        Process the order status notification.
+        """
+        # TODO
--- retail/webhooks/vtex/views/order_status.py
+++ retail/webhooks/vtex/views/order_status.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from sentry_sdk import capture_message
 
 from retail.features.models import IntegratedFeature
 from retail.internal.permissions import CanCommunicateInternally
@@ -21,14 +23,55 @@ class OrderStatusWebhook(APIView):
 
         vtex_account = validated_data.get("vtexAccount")
 
+        project = Project.objects.filter(vtex_account=vtex_account).first()
+
+        order_id = validated_data.get("orderId")
+
+        if not project:
+            error_message = f"Project not found for VTEX account {vtex_account}. Order id: {order_id}"
+            capture_message(error_message)
+
+            raise ValidationError(
+                {"error": "Project not found for this VTEX account"},
+                code="project_not_found",
+            )
+
         domain = OrderStatusUseCase.get_domain_by_account(vtex_account)
-        project = Project.objects.get(vtex_account=vtex_account)
-        integrated_feature = IntegratedFeature.objects.get(
+
+        integrated_feature = IntegratedFeature.objects.filter(
             project=project,
             feature__code="order_status",
+        ).first()
+
+        if not integrated_feature:
+            error_message = f"Order status integration not found for project {project.name}. Order id: {order_id}"
+            capture_message(error_message)
+
+            raise ValidationError(
+                {
+                    "error": "Order status integration not found for this project",
+                },
+                code="order_status_integration_not_found",
+            )
+
+        template_name = OrderStatusUseCase.get_template_by_order_status(
+            validated_data.get("currentState"), integrated_feature
         )
 
-        # TODO: process data
+        if not template_name:
+            error_message = f"Template not found for order status {validated_data.get('currentState')}. Order id: {order_id}"
+            capture_message(error_message)
+
+            raise ValidationError(
+                {
+                    "error": "Template not found for this order status",
+                },
+                code="template_not_found",
+            )
+
+        OrderStatusUseCase.process_notification(
+            domain, validated_data.get("orderId"), template_name
+        )
 
         return Response(
             {
