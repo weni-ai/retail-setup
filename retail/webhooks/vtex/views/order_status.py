3cef5eb3309a41cc4814b8352e4899f98896df40
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-02-07 09:32:48 -0300

    Add response to OrderStatusWebhook

--- retail/webhooks/vtex/views/order_status.py
+++ retail/webhooks/vtex/views/order_status.py
@@ -4,10 +4,24 @@ from rest_framework.request import Request
 from rest_framework import status
 
 from retail.internal.permissions import CanCommunicateInternally
+from retail.webhooks.vtex.serializers import OrderStatusSerializer
 
 
 class OrderStatusWebhook(APIView):
     permission_classes = [CanCommunicateInternally]
 
     def post(self, request: Request) -> Response:
-        pass
+        serializer = OrderStatusSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        validated_data = serializer.validated_data
+
+        # TODO: Add use case
+
+        return Response(
+            {
+                "message": "Order status processed successfully",
+                "order_id": validated_data.get("orderId"),
+            },
+            status=status.HTTP_200_OK,
+        )
