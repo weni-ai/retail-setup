29d366745bf4b28f8d7fe86162f39cfa99043e6c
elitonzky <eliton-jorge@hotmail.com>
2025-02-28 17:06:12 -0300

    add print

--- retail/clients/flows/client.py
+++ retail/clients/flows/client.py
@@ -46,10 +46,12 @@ class FlowsClient(RequestClient, FlowsClientInterface):
         url = f"{self.base_url}/api/v2/internals/whatsapp_broadcasts"
 
         payload = {**payload, "project": project_uuid}
+        print("flows payload", payload)
         response = self.make_request(
             url,
             method="POST",
             json=payload,
             headers=self.authentication_instance.headers,
         )
+        print("flows response", response)
         return response
