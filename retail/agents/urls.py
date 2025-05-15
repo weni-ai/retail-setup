b89c83c78b388c748173b70076c34f0672ad8dfc
Mateus Cavalcanti <mateuscavalcanti1607@gmail.com>
2025-05-15 15:11:51 -0300

    feature: Unassign integrated agent

--- retail/agents/urls.py
+++ retail/agents/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 
 from rest_framework.routers import SimpleRouter
 
-from retail.agents.views import PushAgentView, AgentViewSet, AssignAgentView
+from retail.agents.views import (
+    PushAgentView,
+    AgentViewSet,
+    AssignAgentView,
+    UnassignAgentView,
+)
 
 router = SimpleRouter()
 router.register(r"", AgentViewSet, basename="agents")
@@ -10,6 +15,11 @@ router.register(r"", AgentViewSet, basename="agents")
 urlpatterns = [
     path("push/", PushAgentView.as_view(), name="push-agent"),
     path("<uuid:agent_uuid>/assign/", AssignAgentView.as_view(), name="assign-agent"),
+    path(
+        "<uuid:agent_uuid>/unassign/",
+        UnassignAgentView.as_view(),
+        name="unassign-agent",
+    ),
 ]
 
 urlpatterns += router.urls
