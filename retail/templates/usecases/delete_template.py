58e2b803409c49094d8844becf6e68a5594b2967
Mateus Cavalcanti <mateuscavalcanti1607@gmail.com>
2025-05-28 14:09:55 -0300

    feature: add template slug to ignore template list when deleted

--- retail/templates/usecases/delete_template.py
+++ retail/templates/usecases/delete_template.py
@@ -12,7 +12,13 @@ class DeleteTemplateUseCase:
         except Template.DoesNotExist:
             raise NotFound(f"Template not found: {template_uuid}")
 
+    def _add_template_to_ignore_list(self, template: Template) -> None:
+        integrated_agent = template.integrated_agent
+        integrated_agent.ignore_templates.append(template.parent.slug)
+        integrated_agent.save()
+
     def execute(self, template_uuid: UUID) -> None:
         template = self._get_template(template_uuid)
+        self._add_template_to_ignore_list(template)
         template.is_active = False
         template.save()
