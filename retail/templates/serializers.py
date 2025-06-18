c08a4910caf1cd0891a9ecc8d3d3a17dd58599d3
Mateus Cavalcanti <mateuscavalcanti1607@gmail.com>
2025-06-18 10:49:46 -0300

    fix: staging

--- retail/templates/serializers.py
+++ retail/templates/serializers.py
@@ -106,3 +106,9 @@ class CreateCustomTemplateSerializer(serializers.Serializer):
     integrated_agent_uuid = serializers.CharField(required=True)
     parameters = ParameterSerializer(many=True, required=True)
     display_name = serializers.CharField(required=True)
+
+
+class TemplateMetricsRequestSerializer(serializers.Serializer):
+    template_uuid = serializers.UUIDField(required=True)
+    start = serializers.CharField(required=True)
+    end = serializers.CharField(required=True)
