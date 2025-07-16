608c3d34de99462e24a432fb44256a5142e00c86
Mateus Cavalcanti <mateuscavalcanti1607@gmail.com>
2025-07-16 15:54:53 -0300

    feature: add sts in boto3

--- retail/clients/aws_s3/client.py
+++ retail/clients/aws_s3/client.py
@@ -1,5 +1,7 @@
 import boto3
 
+import logging
+
 from typing import Optional
 
 from django.conf import settings
@@ -7,10 +9,30 @@ from django.core.files.uploadedfile import UploadedFile
 
 from retail.interfaces.clients.aws_s3.client import S3ClientInterface
 
+logger = logging.getLogger(__name__)
+
 
 class S3Client(S3ClientInterface):
     def __init__(self, bucket_name: Optional[str] = None):
-        self.s3 = boto3.client("s3")
+        try:
+            sts_client = boto3.client("sts")
+            assumed_role = sts_client.assume_role(
+                RoleArn=settings.AWS_STORAGE_ROLE, RoleSessionName="S3ClientSession"
+            )
+            credentials = assumed_role["Credentials"]
+
+            self.s3 = boto3.client(
+                "s3",
+                aws_access_key_id=credentials["AccessKeyId"],
+                aws_secret_access_key=credentials["SecretAccessKey"],
+                aws_session_token=credentials["SessionToken"],
+            )
+
+            logger.info("S3Client configured successfully")
+
+        except Exception as e:
+            logger.error(f"Error assuming role {settings.AWS_STORAGE_ROLE}: {e}")
+
         self.bucket_name = bucket_name or getattr(
             settings, "AWS_STORAGE_BUCKET_NAME", "test-bucket"
         )
