2740967a1fc60ec02649d6e39073242c6fc0fa1f
Mateus Cavalcanti <mateuscavalcanti1607@gmail.com>
2025-08-07 11:16:52 -0300

    feat: add support for common users tokens

--- retail/interfaces/clients/connect/interface.py
+++ retail/interfaces/clients/connect/interface.py
@@ -1,8 +1,7 @@
-from typing import Protocol, Dict, Tuple
+from typing import Protocol, Dict, Tuple, Optional
 
 
 class ConnectClientInterface(Protocol):
     def get_user_permissions(
-        self, project_uuid: str, user_email: str
-    ) -> Tuple[int, Dict[str, str]]:
-        ...
+        self, project_uuid: str, user_email: str, user_token: Optional[str] = None
+    ) -> Tuple[int, Dict[str, str]]: ...
