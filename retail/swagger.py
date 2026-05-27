d2d26d649a7e1556cdf05fc1591a75cf2b8e5697
Paulo Bernardo <paulo.bernardo@weni.ai>
2026-05-27 18:51:48 -0300

    chore: release 5.25.0 (#493)

--- retail/swagger.py
+++ retail/swagger.py
@@ -6,7 +6,7 @@ from rest_framework import permissions
 view = get_schema_view(
     openapi.Info(
         title="Gallery API Documentation",
-        default_version="v5.24.1",
+        default_version="v5.25.0",
         description="Documentation of the Gallery APIs",
     ),
     public=True,
